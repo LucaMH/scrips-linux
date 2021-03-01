3550e4c2f8b34cc08bed5760ba42e9c8e4ddf871
LucaMH <luca.hollaender@googlemail.com>
2021-03-01 11:41:22 +0100

    Update systempatch.sh
    sorted lines and added command to check which distro we are running at

--- patching/systempatch.sh
+++ patching/systempatch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#current release 20200301-1136
+#current release 20200301-1140
 
+#get distro
+MYDISTRO=$(hostnamectl | grep "Operating System:")
+MYDISTRO=${MYDISTRO:20}
 
-
-
+#UPDATE APPLIANCES
 #update pihole if existing
 if [ -f /usr/local/bin/pihole ]; then
     pihole -g
@@ -12,7 +14,7 @@ if [ -f /usr/local/bin/pihole ]; then
     sleep 2s
 fi
 
-
+#UPDATES PIP SUFFS
 #update pip if existing (pip2)
 if [ -f /usr/local/bin/pip ]; then
     pip install --upgrade pip
@@ -20,7 +22,6 @@ if [ -f /usr/local/bin/pip ]; then
     pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
-
 
 #update pip3 if existing (Thanks to @Sagamir)
 if [ -f /usr/bin/pip3 ]; then
@@ -31,3 +32,4 @@ if [ -f /usr/bin/pip3 ]; then
 fi
 
 exit
+#todo: add patching for different distros
