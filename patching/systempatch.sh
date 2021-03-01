210a4f9ac209c3993217faa69892f897e088de5c
LucaMH <luca.hollaender@googlemail.com>
2021-03-01 11:35:57 +0100

    Update systempatch.sh
    added pip2

--- patching/systempatch.sh
+++ patching/systempatch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#current release 20200301-11
+#current release 20200301-1135
 
 
 
@@ -9,5 +9,14 @@ if [ -f /usr/local/bin/pihole ]; then
     pihole -g
     sleep 2s
     pihole -up
+    sleep 2s
+fi
+
+
+#update pip if existing (pip2)
+if [ -f /usr/local/bin/pip ]; then
+    pip install --upgrade pip
+    sleep 2s
+    pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
