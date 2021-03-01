4dee221a6a6365d450a317cf8949cb3d03e87ae8
LucaMH <luca.hollaender@googlemail.com>
2021-03-01 17:39:27 +0100

    Update systempatch.sh
    added update commands from the old skript which hat to be modified to the distro

--- linux/systempatch.sh
+++ linux/systempatch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #DISCLAIMER: if this script breaks your system, it is your fault you ran conde before checking the code, always check code which you dont own before running it!
-#current release 20200301-1143
+#current release 20200301-1737
 #GLOBAL SETTINGS
 MYTIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 MYLOGFILE=/tmp/systempatch_scripted__"${MYTIMESTAMP}".log
@@ -55,7 +55,7 @@ fi
 
 if [ ! -f hostnamectl ]; then
     echo exiting du to no hostnamectl
-    #exit
+    exit
 fi
 exit
 #todo: add patching for different distros
@@ -63,5 +63,53 @@ exit
 #distro switcher
 
 
+#update server
+#centos 7
+yum -y update
 
+#centos 8
+dnf update -y -v --refresh
+
+#centos stream
+dnf update -y -v --refresh
+
+#raspbian
+apt -y update
+sleep 2s
+apt -y upgrade
+sleep 2s
+#apt-get dist-upgrade -y #be careful with that
+#sleep 2s
+apt-get clean
+sleep 2s
+apt -y autoremove
+sleep 2s
+apt -y autoclean
+sleep 2s
+sync
+sleep 2s
+
+#debian
+apt-get update -y
+sleep 2s
+apt-get upgrade -y
+sleep 2s
+
+#ubuntu
+apt-get update
+sleep 2s
+apt-get upgrade -y
+sleep 2s
+#apt-get dist-upgrade -y #be careful with that
+apt-get autoremove -y
+sleep 2s
+apt-get autoclean -y
+sleep 2s
+
+#send email to inform updates are done
+#to be implemented
+
+#reboot
+reboot now
+systemctl reboot now
 
