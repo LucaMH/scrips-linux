8aaad1594d8a459090d985f5c0c6e291f5860a4a
LucaMH <luca.hollaender@googlemail.com>
2021-03-01 11:36:47 +0100

    Update systempatch.sh
    added pip3, thanks to Sagamir
    
    Co-Authored-By: Sagamir <8479769+Sagamir@users.noreply.github.com>

--- patching/systempatch.sh
+++ patching/systempatch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#current release 20200301-1135
+#current release 20200301-1136
 
 
 
@@ -20,3 +20,14 @@ if [ -f /usr/local/bin/pip ]; then
     pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
+
+
+#update pip3 if existing (Thanks to @Sagamir)
+if [ -f /usr/bin/pip3 ]; then
+    pip3 install --upgrade pip
+    sleep 2s
+    pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
+    sleep 2s
+fi
+
+exit
