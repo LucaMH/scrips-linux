588e183d0751827df087bd96eaf49b681c9a63eb
LucaMH <luca.hollaender@googlemail.com>
2021-03-01 14:00:52 +0100

    Update systempatch.sh
    skript now talks to you what is is doing

--- patching/systempatch.sh
+++ patching/systempatch.sh
@@ -7,6 +7,7 @@
 if [ -f hostnamectl ]; then
     MYDISTRO=$(hostnamectl | grep "Operating System:")
     MYDISTRO=${MYDISTRO:20}
+    echo I am $MYDISTRO
 else
     echo "no hostname hostnamectl found, will not run systemupdates"
 fi
@@ -14,8 +15,11 @@ fi
 #UPDATE APPLIANCES
 #update pihole if existing
 if [ -f /usr/local/bin/pihole ]; then
+    echo updating pihole appliance
+    echo updating gravity
     pihole -g
     sleep 2s
+    echo updating pihole
     pihole -up
     sleep 2s
 fi
@@ -23,16 +27,22 @@ fi
 #UPDATES PIP SUFFS
 #update pip if existing (pip2)
 if [ -f /usr/local/bin/pip ]; then
+    echo updating pip appliance
+    echo updating pip
     pip install --upgrade pip
     sleep 2s
+    echo updating outdated pip packages
     pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
 
 #update pip3 if existing (Thanks to @Sagamir)
 if [ -f /usr/bin/pip3 ]; then
+    echo updating pip3 appliance
+    echo updating pip3
     pip3 install --upgrade pip
     sleep 2s
+    echo updating outdated pip3 packages
     pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
