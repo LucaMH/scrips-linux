f4230d2d1ce903d66f3a56813cc67c6c19429a49
LucaMH <luca.hollaender@googlemail.com>
2021-03-02 20:51:42 +0100

    Update systempatch.sh

--- linux/systempatch.sh
+++ linux/systempatch.sh
@@ -9,7 +9,6 @@ MYLOGFILE=/tmp/systempatch_scripted__"${MYTIMESTAMP}".log
 #LETS CREATE SUM FUNCTIONS
 
 #get distro
-GETDISTRO () {
 if [ -f /usr/bin/hostnamectl ]
 then
     MYDISTRO=$(hostnamectl | grep "Operating System:")
@@ -19,56 +18,55 @@ then
 else
     echo "no possibility to set the distro found, will not run systemupdates"
 fi
+
+UPDATE_APPLIANCES () {
+    echo running UPDATE_APPLIANCES section now!
+    #update pihole if existing
+
+    if [ -f /usr/local/bin/pihole ] then
+        echo updating pihole appliance
+        echo updating gravity
+        pihole -g
+        sleep 2s
+        echo updating pihole
+        pihole -up
+        sleep 2s
+        echo pihole done
+    else
+        echo there is no pihole
+    fi
+
+
+    #update pip if existing (pip2)
+    if [ -f /usr/local/bin/pip ]; then
+        echo updating pip appliance
+        echo updating pip
+        pip install --upgrade pip
+        sleep 2s
+        echo updating outdated pip packages
+        pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
+        sleep 2s
+        echo  done
+    else
+        echo there is no pip2
+    fi
+
+    #update pip3 if existing (Thanks to @Sagamir)
+    if [ -f /usr/bin/pip3 ]; then
+        echo updating pip3 appliance
+        echo updating pip3
+        pip3 install --upgrade pip
+        sleep 2s
+        echo updating outdated pip3 packages
+        pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
+        sleep 2s
+    else
+        echo there is no pip3
+    fi
 }
 
-#UPDATE APPLIANCES
-#update pihole if existing
-UPDATE_PIHOLE () {
-if [ -f /usr/local/bin/pihole ] then
-    echo updating pihole appliance
-    echo updating gravity
-    pihole -g
-    sleep 2s
-    echo updating pihole
-    pihole -up
-    sleep 2s
-    echo pihole done
-else
-    echo there is no pihole
-fi
-}
+UPDATE_APPLIANCES ()
 
-#update pip if existing (pip2)
-UPDATE_PIP2 () {
-if [ -f /usr/local/bin/pip ]; then
-    echo updating pip appliance
-    echo updating pip
-    pip install --upgrade pip
-    sleep 2s
-    echo updating outdated pip packages
-    pip list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
-    sleep 2s
-    echo  done
-else
-    echo there is no pip2
-fi
-}
-#update pip3 if existing (Thanks to @Sagamir)
-UPDATE_PIP3 () {
-if [ -f /usr/bin/pip3 ]; then
-    echo updating pip3 appliance
-    echo updating pip3
-    pip3 install --upgrade pip
-    sleep 2s
-    echo updating outdated pip3 packages
-    pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
-    sleep 2s
-else
-    echo there is no pip3
-fi
-}
-
-GETDISTRO
 exit
 #UPDATE_PIHOLE
 #UPDATE_PIP2
