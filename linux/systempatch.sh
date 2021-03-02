b1d11bd4e7310739c4ee5295857252f0daa05c51
LucaMH <luca.hollaender@googlemail.com>
2021-03-02 20:26:52 +0100

    Update systempatch.sh
    added functions

--- linux/systempatch.sh
+++ linux/systempatch.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 #DISCLAIMER: if this script breaks your system, it is your fault you ran conde before checking the code, always check code which you dont own before running it!
-#current release 20200301-1737
+#current release 20200302-2026
 #GLOBAL SETTINGS
 MYTIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 MYLOGFILE=/tmp/systempatch_scripted__"${MYTIMESTAMP}".log
 
-#get distro
+#LETS CREATE SUM FUNCTIONS
 
-if [ -f hostnamectl ]; then
+#get distro
+function (GETDISTRO) {
+if [ -f /usr/bin/hostnamectl ]
+then
     MYDISTRO=$(hostnamectl | grep "Operating System:")
     MYDISTRO=${MYDISTRO:20}
     echo I am $MYDISTRO
+#elif [ -f other  ]
 else
-    echo "no hostname hostnamectl found, will not run systemupdates"
+    echo "no possibility to set the distro found, will not run systemupdates"
 fi
+}
 
 #UPDATE APPLIANCES
 #update pihole if existing
-if [ -f /usr/local/bin/pihole ]; then
+function (UPDATE_PIHOLE) {
+if [ -f /usr/local/bin/pihole ] then
     echo updating pihole appliance
     echo updating gravity
     pihole -g
@@ -27,9 +33,10 @@ if [ -f /usr/local/bin/pihole ]; then
     sleep 2s
     echo pihole done
 fi
+}
 
-#UPDATES PIP SUFFS
 #update pip if existing (pip2)
+function (UPDATE_PIP2) {
 if [ -f /usr/local/bin/pip ]; then
     echo updating pip appliance
     echo updating pip
@@ -40,8 +47,9 @@ if [ -f /usr/local/bin/pip ]; then
     sleep 2s
     echo  done
 fi
-
+}
 #update pip3 if existing (Thanks to @Sagamir)
+function (UPDATE_PIP3) {
 if [ -f /usr/bin/pip3 ]; then
     echo updating pip3 appliance
     echo updating pip3
@@ -51,12 +59,15 @@ if [ -f /usr/bin/pip3 ]; then
     pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
     sleep 2s
 fi
+}
 
-
-if [ ! -f hostnamectl ]; then
+if [ ! -f /usr/bin/hostnamectl ]
+then
     echo exiting du to no hostnamectl
-    exit
+    #exit
 fi
+
+
 exit
 #todo: add patching for different distros
 
@@ -110,6 +121,8 @@ sleep 2s
 #to be implemented
 
 #reboot
+echo script finished
+echo rebooting now
 reboot now
 systemctl reboot now
 
